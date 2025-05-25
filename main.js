97a73636ed0836d347e18f8d22557e54ff7a2db9
EventosGDN <eventos@gmail.com>
2025-05-25 20:32:51 -0300

    cambio 79

--- main.js
+++ main.js
@@ -670,9 +670,9 @@ chatContainer.addEventListener('scroll', () => {
 
 
 
-document.addEventListener('DOMContentLoaded', () => {
+//document.addEventListener('DOMContentLoaded', () => {
   // Esto mete 10 mensajes de prueba al chat para que scrollee
-  for (let i = 0; i < 10; i++) {
-    addMessage('Mensaje de prueba ' + (i+1), 'original');
-  }
-});
+  //for (let i = 0; i < 10; i++) {
+    //addMessage('Mensaje de prueba ' + (i+1), 'original');
+  //}
+//});
