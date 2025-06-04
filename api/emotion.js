09b11ba0caeadd87c2ce57a696f330be2daf2f2b
EventosGDN <eventos@gmail.com>
2025-06-04 02:13:43 -0300

    version estable 8

--- api/emotion.js
+++ api/emotion.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+/* export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Método no permitido' });
   }
@@ -45,3 +45,4 @@ Mensaje: "${text}"
     return res.status(500).json({ error: 'Error interno al detectar emoción' });
   }
 }
+ */
